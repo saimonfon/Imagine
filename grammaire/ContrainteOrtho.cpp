1a3f8a8bf4c08e3f41d452ae275023a961bb7c2e
unknown <simon@bechet.(none)>
2012-06-05 14:59:21 +0200

    finak

---
+++ grammaire/ContrainteOrtho.cpp
@@ -0,0 +1,17 @@
+#include "condition/ConditionGenerale.h"
+#include "parsing/Noeud.h"
+#include <vector>
+#include <iostream>
+//La contrainte pour une fenetre adjacence exacte (égalité des sommets)
+class Contrainte : public ConditionGenerale
+{
+public: 
+bool estVerifiee(vector<Noeud*> enfants, Parser* p)
+{
+	Polygone* p1 = (Polygone*) enfants[0]->getAttribut("primitive");
+	Polygone* p2 = (Polygone*) enfants[1]->getAttribut("primitive");
+	Vec3 n1(p1->equation[0],p1->equation[1],p1->equation[2]);
+	Vec3 n2(p2->equation[0],p2->equation[1],p2->equation[2]);
+	return n1*n2<0.1;
+}
+};
